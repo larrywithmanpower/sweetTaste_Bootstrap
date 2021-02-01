01140f202e30954d30ca5fc9870268609d5aa2f2
larrywithmanpower <48421515+larrywithmanpower@users.noreply.github.com>
2021-02-01 13:27:59 +0800

    first update file

---
+++ source/javascripts/all.js
@@ -0,0 +1,13 @@
+
+// remove modal
+// 當Modal顯示出來時作動
+$("#removeModal").on("show.bs.modal", function (event) {
+    // 取得點擊的btn
+    var btn = $(event.relatedTarget);
+    // 取出按鈕的值(取出data-title)
+    var title = btn.data("title");
+    // 將title寫回Modal
+    var modal = $(this);
+    // modal找到本身的class name .modal-title，使用.text(title)替換成中文字
+    modal.find(".modal-title").text("確認刪除" + title);
+});
